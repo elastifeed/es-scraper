851ff8d23714687dc87b8e0f72024731004b6139
Matthias Riegler <me@xvzf.tech>
2019-05-02 13:03:21 +0200

    Split up in /parse/html and /parse/url, corrected async await syntax

--- mercury-docker/main.js
+++ mercury-docker/main.js
@@ -1,36 +1,38 @@
 const Mercury = require('@postlight/mercury-parser');
 const express = require('express');
 const header = {'User-Agent':'Googlebot/2.1 (+http://www.google.com/bot.html)'}; // Always set User Agent in Header
+
 var app = express(); // Use express to set up a basic server
-
 app.use(express.json({limit : '100mb'}));
-// React to post on /parse
-app.post("/parse", function(request, response) {
-    data = request.body;
 
-    if (data.url == null && data.html == null) {
-        response.status(403);
-        sendResult(response, requestError());
-    } else if (data.html == null) { // No preloaded HTML specified - Parse from URL.
-        console.log("Parsing from url:")
-        console.log(data.url)
-        Mercury.parse(data.url, {headers : header})
-            .then(result => sendResult(response, result));
-    } else {
-        console.log("Parsing from html:")
-        console.log(data.url)
-        Mercury.parse(data.url, {headers : header, contentType : 'text', html : data.html})
-           .then(result => sendResult(response, result));
+// React to post on /parse/url
+app.post("/parse/url", async function(req, res) {
+    const data = req.body;
+
+    if (!data.url) {
+        return res.status(400).send({
+            error: 1,
+            message: "Invalid JSON object"
+        });
     }
+
+    console.log(`Parsing from url: ${data.url}`)
+    return res.json(await Mercury.parse(data.url, {headers : header}))
+
+});
+
+// React to post on /parse/html
+app.post("/parse/html", async function(req, res) {
+    const data = req.body;
+
+    if (!data.url && !data.html) {
+        return res.status(400).send({
+            error: 1,
+            message: "Invalid JSON object"
+        });
+    }
+    console.log(`Parsing from html: ${data.url}`)
+    res.json(await Mercury.parse(data.url, {headers : header, contentType : 'text', html : data.html}))
 });
 
 app.listen(8080);
-
-function sendResult(response, data) {
-    console.log("Sending: " + data)
-    response.json(data); // Send the data
-}
-
-function requestError() {
-    return {'error':1, 'message':'Invalid JSON object.'};
-}
