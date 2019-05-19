7901046450c43835003505b5854b7921afe7f43f
Mark Martinssen <martinussen.mark@gmail.com>
2019-05-19 17:04:14 +0200

    Split raw and markdown formatted content

--- mercury-docker/main.js
+++ mercury-docker/main.js
@@ -1,41 +1,60 @@
 const Mercury = require('@postlight/mercury-parser');
 const express = require('express');
+const TurndownService = require('turndown');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 const header = {'User-Agent':'Googlebot/2.1 (+http://www.google.com/bot.html)'}; // Always set User Agent in Header
 
 var app = express(); // Use express to set up a basic server
 app.use(express.json({limit : '100mb'}));
 
 // React to post on /parse/url
-app.post("/parse/url", async function(req, res) {
+app.post("/mercury/url", async function(req, res) {
     const data = req.body;
-    console.log(data)
 
-    if (!data.url) {
+    if (!data.url) { // POST didn't have a url
         return res.status(400).send({
             error: 1,
             message: "Invalid JSON object"
         });
     }
 
-    console.log(`Parsing from url: ${data.url}`)
-    res.json(await Mercury.parse(data.url, {headers : header, contentType : 'markdown'}))
+    console.log(`Parsing from url: ${data.url}`);
+    let parsed = await Mercury.parse(data.url, {headers : header, contentType : 'html'});
+    parsed = await extractTextFromHtml(parsed); // Use turndown and querySelectors to retrieve markdown & plain text
+    res.json(parsed);
 
 });
 
 // React to post on /parse/html
-app.post("/parse/html", async function(req, res) {
+app.post("/mercury/html", async function(req, res) {
     const data = req.body;
 
-    if (!data.url || !data.html) {
+    if (!data.url || !data.html) { // POST didn't have url or html
         return res.status(400).send({
             error: 1,
             message: "Invalid JSON object"
         });
     }
-    console.log(`Parsing from html: ${data.url}`)
-    let test = await Mercury.parse(data.url, {headers : header, contentType : 'markdown', html : data.html})
-    console.log(test.content)
-    res.json(test)
+    console.log(`Parsing from html: ${data.url}`);
+    let parsed = await Mercury.parse(data.url, {headers : header, contentType : 'html', html : data.html});
+    parsed = await extractTextFromHtml(parsed)
+    res.json(parsed);
 });
+
+/**
+ * Takes the parsed html and extracts plain text and markdown from it.
+ * @param {JSON} parsedJson The parsed json containing a "content" field.
+ */
+async function extractTextFromHtml(parsedJson) {
+    const dom = await new JSDOM(parsedJson.content);
+    const md = await new TurndownService();
+
+    await delete parsedJson['content'];
+    parsedJson.raw_content = dom.window.document.body.textContent;
+    parsedJson.markdown_content = md.turndown(dom.window.document); // Turndown library for markdown
+
+    return parsedJson;
+}
 
 app.listen(8080);
