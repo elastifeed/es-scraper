53c15332ed5ab076b9b06bd7d1be4966c6abbcc8
Mark Martinssen <martinussen.mark@gmail.com>
2019-05-24 12:13:55 +0200

    Fixed bug where checking if the url was already loaded failed

--- retrieval.py
+++ retrieval.py
@@ -1,4 +1,5 @@
 import asyncio
+import difflib
 from uuid import uuid4
 from crawler import Crawler
 from pyppeteer import launch
@@ -17,7 +18,7 @@ class Retrieval:
     @staticmethod
     async def initialize():
         Retrieval.browser = await launch()
-        p = (await Retrieval.browser.pages())[0]
+        p = await Retrieval._get_page()
         # Use GoogleBot UserAgent to handle redirections
         await p.setUserAgent("Googlebot/2.1 (+http://www.google.com/bot.html)")
         p.on("dialog", lambda dialog: asyncio.ensure_future(Retrieval._dismiss_dialog(dialog)))
@@ -34,9 +35,20 @@ class Retrieval:
         """ Gets the page that in the browser has loaded for this url.
         :param url: the url of the page
         """
-        p : Page = (await Retrieval.browser.pages())[0]
 
-        if p.url is url:
+        def __url_compare(expected, actual):
+            """Compares two urls and determines whether they are the same. The need for this is because for example http://google.de and https://www.google.de/ are the same adress and thus should'nt be loaded twice
+            """
+            allowed = ['w', 's', '/', '.']
+            diff = [change for change in difflib.ndiff(actual, expected) if change[0] != ' ']
+            for i in diff:
+                if i[-1] not in allowed: # Checking the last char, if it isn't part of the exclusion list, we can't guarantee that the urls are the same.
+                    return False
+            return True
+            
+        p : Page = await Retrieval._get_page()
+
+        if __url_compare(p.url, url):
             return p # Page with correct url already loaded
         else:
             # Wait until domcontentloaded event is fired, otherwise, parsing results may be inconsitent
@@ -44,6 +56,16 @@ class Retrieval:
             await p.goto(url, {'waitUntil': 'domcontentloaded'})
 
             return p
+
+    @staticmethod
+    async def _get_page():
+        """Retrieves an already initialized page object of the browser.
+        :raises RetrievalException: If the browser has no pages."""
+        for target in Retrieval.browser.targets():
+            if target.type == "page":
+                return await target.page()
+        # No page found
+        raise RetrievalException('Browser contains no pages.')
 
     @staticmethod
     async def _dismiss_dialog(dialog):
@@ -57,7 +79,7 @@ class Retrieval:
         try:  # Assert that we have the necessary information to proccess the request
             page = await Retrieval._load(url)
         except AttributeError as e:
-            logger.exception(e)
+            logger.warning(e, exc_info=True)
             raise RetrievalException(e)
         except NetworkError as e:
             logger.error(e)
@@ -78,7 +100,7 @@ class Retrieval:
             await screenshot(page, path)
             return path
         except AttributeError as e:
-            logger.exception(e)
+            logger.warning(e, exc_info=True)
             raise RetrievalException(e)
         except NetworkError as e:
             logger.error(e)
@@ -92,7 +114,7 @@ class Retrieval:
             await renderPdf(page, path)
             return path
         except AttributeError as e:
-            logger.exception(e)
+            logger.warning(e, exc_info=True)
             raise RetrievalException(e)
         except NetworkError as e:
             logger.error(e)
